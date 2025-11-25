bb905c028d96fdf86fd2361206a202307d6eec30
SamJ-0 <210594751+SamJ-0@users.noreply.github.com>
2025-11-25 17:26:08 +0000

    Added a README, a write to file function and the nested object code.

---
+++ README.md
@@ -0,0 +1,5 @@
+# Description
+
+This project is aimed at learning Axios, Cheerio, more Node.js, navigating the HTML DOM as well as cleaning and adding data to an SQL database.
+
+This is a simple application that gathers a product's nutritional data per 100g and per serving.
--- index.js
+++ index.js
@@ -4,19 +4,30 @@ import fs from "fs";
 
 const url = "";
 
-const foodData = {};
+const foodData = { name: "", per100g: {}, perServing: {} };
 
 async function nutritionScrape() {
-  const { data: html } = await axios.get(url);
-  const $ = cheerio.load(html);
+  try {
+    const { data: html } = await axios.get(url);
+    const $ = cheerio.load(html);
 
-  $(".nutrition-table tr").each((i, row) => {
-    const nutritionData = $(row).find("td").text();
-    const label = $($(row).find("td")[0]).text();
-    const valuesPer100g = $($(row).find("td")[1]).text();
-    const valuesPerServing = $($(row).find("td")[2]).text();
-    console.log(`${label}: ${valuesPer100g}`);
-  });
+    const productName = $(".product-heading").find("h1").text();
+    foodData["name"] = productName;
+
+    $(".nutrition-data tr").each((i, row) => {
+      const label = $($(row).find("td")[0]).text();
+      const valuesPer100g = $($(row).find("td")[1]).text();
+      const valuesPerServing = $($(row).find("td")[2]).text();
+      foodData["per100g"][label] = valuesPer100g;
+      foodData["perServing"][label] = valuesPerServing;
+    });
+
+    fs.writeFileSync("FoodData.json", JSON.stringify(foodData));
+
+    console.log(foodData);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 nutritionScrape();
